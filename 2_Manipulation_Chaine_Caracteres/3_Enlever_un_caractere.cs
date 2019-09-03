25b15f24b533a9d66faf586274416f351e27bf22
Adrien <adrien.mechin@gmail.com>
2019-09-03 11:22:46 +0200

    Manipulation chaine - enlever un caractère

---
+++ 2_Manipulation_Chaine_Caracteres/3_Enlever_un_caractere.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnleverLesA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chaine = null, temp = null;
+            int taille = 0;
+            do
+            {
+                Console.WriteLine("Saisir une phrase (100 caractères maximum)");
+                chaine = Console.ReadLine().ToString();
+                taille = chaine.Length;
+            }
+            while (taille > 100);
+
+            foreach (char c in chaine)
+            {
+                if(c != 'a' && c != 'A')
+                {
+                    temp += c.ToString();
+                }
+            }
+
+            
+            Console.WriteLine("La phrase sans le 'A' est : \n\t -> " + temp);
+            Console.ReadLine();
+        }
+    }
+}
